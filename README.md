1afb974e96254a5ee888544b5a3be4f0eaff6303
dianahub250 <niyongogadidi@gmail.com>
2021-02-02 09:00:24 -1200

    retrieving data from postman to table

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # flutter_responsive_table
+
+## Project Desrciption
 A simple flutter project to fetch data from postman and display them in table
+
+## Project Owner
+<a href="https://github.com/dianahub250">dianahub250</a>
+
+## How should it be tested?
+
+- Clone it from github
+- Run <b> flutter pub get</b> command to install dependecies
+- Run it using <b> flutter run </b>
+
+
+## Screenshots
+
+<img src="lib/assets/images/Capture.png"></img><br>
+
+
---
+++ lib/table.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: DataTableDemo(),
+    );
+  }
+}
+
+class DataTableDemo extends StatefulWidget {
+  @override
+  _DataTableDemoState createState() => _DataTableDemoState();
+}
+
+class _DataTableDemoState extends State<DataTableDemo> {
+  final String url = "https://fakerapi.it/api/v1/users?_quantity=100";
+  int index = 0;
+  List result;
+  @override
+  void initState() {
+    super.initState();
+    this.getJsonData();
+  }
+
+  Future<String> getJsonData() async {
+    var response = await http.get(url, headers: {"Accept": "application/json"});
+    print(response.body);
+
+    setState(() {
+      var convertDataToJson = json.decode(response.body);
+      result = convertDataToJson['data'];
+    });
+    return "Success";
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Data Tables'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(16),
+        children: [
+          PaginatedDataTable(
+            header: Text('Header Text'),
+            rowsPerPage: 4,
+            columns: [
+              DataColumn(label: Text('Header A')),
+              DataColumn(label: Text('Header B')),
+              DataColumn(label: Text('Header C')),
+              DataColumn(label: Text('Header D')),
+            ],
+            source: _DataSource(context),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Row {
+  _Row(
+    this.valueA,
+    this.valueB,
+    this.valueC,
+    this.valueD,
+  );
+
+  final String valueA;
+  final String valueB;
+  final String valueC;
+  final int valueD;
+
+  bool selected = false;
+}
+
+class _DataSource extends DataTableSource {
+  _DataSource(this.context) {
+    _rows = <_Row>[
+      _Row('Cell A1', 'CellB1', 'CellC1', 1),
+      _Row('Cell A2', 'CellB2', 'CellC2', 2),
+      _Row('Cell A3', 'CellB3', 'CellC3', 3),
+      _Row('Cell A4', 'CellB4', 'CellC4', 4),
+    ];
+  }
+
+  final BuildContext context;
+  List<_Row> _rows;
+
+  int _selectedCount = 0;
+
+  @override
+  DataRow getRow(int index) {
+    assert(index >= 0);
+    if (index >= _rows.length) return null;
+    final row = _rows[index];
+    return DataRow.byIndex(
+      index: index,
+      selected: row.selected,
+      onSelectChanged: (value) {
+        if (row.selected != value) {
+          _selectedCount += value ? 1 : -1;
+          assert(_selectedCount >= 0);
+          row.selected = value;
+          notifyListeners();
+        }
+      },
+      cells: [
+        DataCell(Text(row.valueA)),
+        DataCell(Text(row.valueB)),
+        DataCell(Text(row.valueC)),
+        DataCell(Text(row.valueD.toString())),
+      ],
+    );
+  }
+
+  @override
+  int get rowCount => _rows.length;
+
+  @override
+  bool get isRowCountApproximate => false;
+
+  @override
+  int get selectedRowCount => _selectedCount;
+}
